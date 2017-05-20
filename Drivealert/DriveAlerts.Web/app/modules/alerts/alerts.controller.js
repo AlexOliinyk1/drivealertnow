5214776e340f75ddc38c2d8a95e2eac719f833c1
alex <alex.oliinyk1@gmail.com>
2017-05-20 19:05:29 +0300

    added devices service

--- Drivealert/DriveAlerts.Web/app/modules/alerts/alerts.controller.js
+++ Drivealert/DriveAlerts.Web/app/modules/alerts/alerts.controller.js
@@ -1,30 +1,38 @@
-﻿app.controller('AlertsController', ['$scope', 'AlertService',
-    function ($scope, alertService) {
+﻿app.controller('AlertsController', ['$scope', 'AlertService', 'DevicesService',
+    function ($scope, alertService, devicesService) {
         var vm = this;
 
         vm.phones = [];//added phones : need to take this from api
         vm.selectedPhone = '';
+        vm.alerts = [];
         vm.searchConfig = {
-            DateStart: "2017-05-20T10:17:20.315Z",
-            DateEnd: "2017-05-20T10:17:20.315Z",
+            DateStart: "2017-05-18T0:00:00.1Z",
+            DateEnd: "2017-05-21T0:00:00.1Z",
             IsSpeedAlerts: true,
             IsEmergencyCalls: true,
             IsConnectionViolations: true,
             IsCallViolationAlerts: true
         };
-        vm.alerts = [];
 
         //  get phones for user
         function _loadPhones() {
-            vm.phones.push('1111111111');
-            vm.selectedPhone = vm.phones[0];
+            devicesService.getDevices()
+                .then(function (result) {//success
+                    vm.phones.push(result);
+                    vm.selectedPhone = vm.phones[0];
+                }, function () {//fail
+                });
         }
 
         //  get alerts for selected phone
         function _searchAlerts() {
             alertService.getAlerts(vm.selectedPhone, vm.searchConfig)
                 .then(function (result) {//success
+                    debugger;
                     vm.alerts = result.data.Alerts;
+                    if (vm.alerts.length == 0) {
+                        _set_test_alerts();
+                    }
                 }, function (exc) {//fail
                     console.log(exc);
                 });
@@ -33,8 +41,10 @@
         vm.searchAlerts = _searchAlerts;
         _loadPhones();
 
-        $scope.$watch('vm.selectedPhone', function (newValue, oldValue) {
-            _searchAlerts();
+        $scope.$watch('vm.selectedPhone', function (current, original) {
+            if (current !== '') {
+                _searchAlerts();
+            }
         });
     }
 ]);
