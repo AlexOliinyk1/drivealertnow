c5253c9af4ff99d365c5e33e1a09737970d7347a
alex <alex.oliinyk1@gmail.com>
2017-05-20 22:45:17 +0300

    added direstive for bootstrap tabs

---
+++ Drivealert/DriveAlerts.Web/app/directives/show-tab.directive.js
@@ -0,0 +1,10 @@
+﻿app.directive('showTab', function () {
+    return {
+        link: function (scope, element, attrs) {
+            element.click(function (e) {
+                e.preventDefault();
+                jQuery(element).tab('show');
+            });
+        }
+    };
+});
