c709c34c1b8676049326533a7b84885b813fea76
alex <alex.oliinyk1@gmail.com>
2017-05-21 14:04:22 +0300

    refactoring Devices logic

--- Drivealert/DriveAlerts.Web/app/modules/devices/devices.controller.js
+++ Drivealert/DriveAlerts.Web/app/modules/devices/devices.controller.js
@@ -3,12 +3,41 @@
         var vm = this;
         vm.text = "Devices";
         vm.phones = [];
+        vm.deleteDevice = _deleteDevice;
+        vm.openEditor = _openEditor;
         vm.$onInit = _loadDevices;
 
         function _loadDevices() {
             devicesService.getDevices()
                 .then(function (result) {//success
                     vm.phones = result;
+                });
+        }
+
+        function _deleteDevice() {
+            devicesService.removeDevice(id)
+                .then(function (result) {
+                    if (result == 200) {
+
+                    } else { 
+                        console.log(result);
+                    }
+                });
+        }
+
+        function _openEditor(id) {
+            alert('need to implement Edit. id = ' + id);
+        }
+
+        //  todo: move this to updata controller
+        function _updataDevice(device) {
+            devicesService.updateDevice(device.PhoneId, device)
+                .then(function (result) {
+                    if (result == 200) {
+
+                    } else { 
+                        console.log(result);
+                    }
                 });
         }
 
--- Drivealert/DriveAlerts.Web/app/services/devices.service.js
+++ Drivealert/DriveAlerts.Web/app/services/devices.service.js
@@ -1,8 +1,9 @@
 ﻿app.factory('DevicesService', ['$http', '$q', 'ngWebSettings', 'AuthService',
     function ($http, $q, ngWebSettings, authService) {
-        var serviceBase = ngWebSettings.apiServiceBaseUri + '/';
+        var serviceBase = ngWebSettings.apiServiceBaseUri;
         var service = {};
 
+        //  GET /phones/user/{userId}
         function _getDevices() {
             var url = serviceBase + 'phones/user/' + authService.authentication.userId;
             return $http.get(url, { headers: { 'Content-Type': 'application/json' } })
@@ -11,12 +12,40 @@
                 });
         }
 
-        function _createDevice(device) {
-            return $http.post(serviceBase + 'accounts/phones', device, { headers: { 'Content-Type': 'application/json' } });
+        //  POST /phones/user/{userId}
+        function _createDevice(userId, device) {
+            var url = serviceBase + 'phones/user/' + authService.authentication.userId;
+
+            return $http.post(url, device, { headers: { 'Content-Type': 'application/json' } })
+                .then(function (result) {
+
+                });
+        }
+
+        //  PUT /phones/{phoneId}
+        function _updateDevice(id, device) {
+            return $http.put(serviceBase + 'phones/' + id, device, { headers: { 'Content-Type': 'application/json' } })
+                .then(function (result) {
+                    return result.status
+                }).catch(function (error) {
+                    return 500;
+                });
+        }
+
+        //  DELETE /phones
+        function _removeDevice(id) {
+            return $http.put(serviceBase + 'phones', id, { headers: { 'Content-Type': 'application/json' } })
+                .then(function (result) {
+                    return result.status
+                }).catch(function (error) {
+                    return 500;
+                });
         }
 
         service.getDevices = _getDevices;
         service.createDevice = _createDevice;
+        service.updateDevice = _updateDevice;
+        service.removeDevice = _removeDevice;
         return service;
     }
 ]);
