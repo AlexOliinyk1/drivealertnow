f8b5b0e3a994203461d546b8e3ab6e2a060cca1f
alex <alex.oliinyk1@gmail.com>
2017-05-30 15:47:28 +0300

    added directive for collapse/expand buttons icon

---
+++ Drivealert/DriveAlerts.Web/app/directives/bootstrap-collapse-btn.js
@@ -0,0 +1,22 @@
+﻿app.directive('bootstrapCollapseBtn', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var expanded = 'fa-minus';
+            var collapsed = 'fa-plus';
+
+            element.click(function (e) {
+                var i = element.find('i');
+
+                if (i.hasClass(expanded)) {
+                    i.removeClass(expanded);
+                    i.addClass(collapsed);
+                }
+                else {
+                    i.removeClass(collapsed);
+                    i.addClass(expanded);
+                }
+            });
+        }
+    };
+});
