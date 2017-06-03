b64d0dd14f94c4e05b4673aca7c3ff4c9f5b1a95
alex <alex.oliinyk1@gmail.com>
2017-06-03 17:50:56 +0300

    signup wizzard step1

--- Drivealert/DriveAlerts.Web/app/modules/signup-wizard/signup-wizard.controller.js
+++ Drivealert/DriveAlerts.Web/app/modules/signup-wizard/signup-wizard.controller.js
@@ -1,4 +1,15 @@
-﻿app.controller('SignupWizardController', [function () {
-    var vm = this;
-    
-}]);
+﻿app.controller('SignupWizardController', ['$scope',
+    function ($scope) {
+        var vm = this;
+
+        vm.step1 = {
+            email: '',
+            orderNumber: '',
+            yesNoQuestion: [{ text: 'Yes', value: true }, { text: 'No', value: false }],
+            isHavePromotionCode: false,
+            promotionCode: '',
+            isPartOfSafetyProgram: false
+        };
+
+    }
+]);
