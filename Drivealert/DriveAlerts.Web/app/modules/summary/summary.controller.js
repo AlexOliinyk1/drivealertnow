c93e6023a372a409a7f68d61ece42dbcfb003439
alex <alex.oliinyk1@gmail.com>
2017-06-12 19:14:09 +0300

    fixed date range selection on summary

--- Drivealert/DriveAlerts.Web/app/modules/summary/summary.controller.js
+++ Drivealert/DriveAlerts.Web/app/modules/summary/summary.controller.js
@@ -1,18 +1,32 @@
 ﻿app.controller('SummaryController', ['$scope', 'SummaryService', 'BufferService',
     function ($scope, summaryService, bufferService, authService, devicesService) {
         var vm = this;
+        var filter = {
+            fromDate: new Date(),
+            toDate: new Date()
+        };
 
         vm.currentPhone = null;
         vm.isLoading = {};
         vm.summaryReport = [];
-        vm.filter = {
+        vm.filterUI = {
             fromDate: new Date(),
             toDate: new Date()
         };
         vm.updateData = _loadReport;
 
+        $scope.$watch('vm.filterUI.fromDate', function (current, original) {
+            vm.filter.fromDate = new Date(current);
+            vm.filter.fromDate.setHours(0, 0, 0, 0);
+        });
+
+        $scope.$watch('vm.filterUI.toDate', function (current, original) {
+            vm.filter.toDate = new Date(current);
+            vm.filter.toDate.setHours(0, 0, 0, 0);
+        });
+
         function _loadReport() {
-            vm.isLoading = summaryService.getSummary(vm.filter.fromDate, vm.filter.toDate)
+            vm.isLoading = summaryService.getSummary(filter.fromDate, filter.toDate)
                 .then(function (result) {
                     console.log(result);
                     vm.summaryReport = result;
